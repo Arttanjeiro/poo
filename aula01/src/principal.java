7fc6c6263643036f19336413267a33904aebe869
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-02-03 17:46:37 -0300

    Arrumando -as pasta

---
+++ aula01/src/principal.java
@@ -0,0 +1,15 @@
+/* Essa atividade é pra fazer uma class que tem todos os atributos do aluno, como media do 
+ * aluno e situação do aluno;
+ */
+
+ import Aluno.Aluno;
+
+ public class principal {
+   
+   public static void main(String[] args) {
+     Aluno aluno1 = new Aluno(" Arthur", 0,10);
+ 
+     System.out.println( aluno1.toString());
+   }
+ }
+ 
